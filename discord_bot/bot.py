5b1a13bb3a30ad16451dd291da0a326b5e9e8cdc
sohuh <gavinhamon1234@gmail.com>
2026-07-15 12:34:13 -0400

    syntax fix

--- discord_bot/bot.py
+++ discord_bot/bot.py
@@ -18,20 +18,20 @@ async def on_ready():
         print("Logged in as a bot {0.user}".format(client))
 
 @client.event
-asycn def on_message(message):
+async def on_message(message):
     username = str(message.author).split("#")[0]
     channel = str(message.channel.name)
     user_message = str(message.content)
 
     #similar to printf(); in C
-    print(f'Message {user_message} by {username on {channel}')
+    print(f'Message {user_message} by {username} on {channel}')
 
-    if message.author = client.user:
+    if message.author == client.user:
         return
 
-    if channel == "random"
+    if channel == "random":
         if user_message.lower() == "hello" or user_message.lower() == "hi":
-            await message.channel.sned(f'Hello {username')
+            await message.channel.send(f'Hello {username}')
             return
         elif user_message.lower == "bye":
             await message.channel.send(f'Bye {username}')
